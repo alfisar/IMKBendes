143e4ceed9da241913665cd1599a6c3f37782122
alfisar <alfisar589@gmail.com>
2018-11-19 23:07:42 +0700

    update data base dan fix login

--- modabase/login.php
+++ modabase/login.php
@@ -5,8 +5,6 @@
 		$query = mysqli_query($conn,"SELECT * FROM `akun` WHERE `email` = '".$_POST['email']."' and `password` = '".$_POST['pass']."'");
 		$data = mysqli_fetch_array($query);
 		if ($data['email']==$_POST['email'] && $data['password']==$_POST['pass'] && $_POST['email']!=""){
-			$_SESSION['nama'] = $data['namad'];
-			$_SESSION['namab'] = $data['namab'];
 			$_SESSION['email'] = $data['email'];
 			header("Location: index-dashboard.php");
 		}
